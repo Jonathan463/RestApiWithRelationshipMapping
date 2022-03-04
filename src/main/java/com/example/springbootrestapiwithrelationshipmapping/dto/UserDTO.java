f31eb7072f98a6fe2a4b647e912a4385bb1dfbca
decagon <godsonjonathan39@gmail.com>
2022-03-04 23:29:22 +0100

    Update

--- src/main/java/com/example/springbootrestapiwithrelationshipmapping/dto/UserDTO.java
+++ src/main/java/com/example/springbootrestapiwithrelationshipmapping/dto/UserDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserDTO {
     private String firstName;
-    private String lastName;
+    private String LastName;
     private String userName;
     private String email;
     private String location;
